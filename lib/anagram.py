99fe646814df4398271e939043a9f4baa86711e7
Jasec <jasec_7@hotmail.com>
2025-08-06 02:11:43 -0700

    complete python p3 anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def word(self):
+        return self._word 
+    
+    @word.setter
+    def word(self, word):
+        if word == word:
+            self._word = word.lower()
+
+    def match(self, word_list):
+        candidates = []
+        for n in word_list:
+            if sorted(n) == sorted(self.word):
+                candidates.append(n)
+        return candidates
